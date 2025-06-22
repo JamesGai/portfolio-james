703c955ec8ac4e23f7da7e5d3f9712f45f4a7209
JamesGai <james1412049013@gmail.com>
2025-06-22 14:27:17 +1200

    Completed about page

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -8,8 +8,6 @@ import {
   IonGrid,
   IonRow,
   IonCol,
-  IonModal,
-  IonButton,
 } from '@ionic/angular/standalone';
 
 @Component({
@@ -26,41 +24,6 @@ import {
     IonGrid,
     IonRow,
     IonCol,
-    IonModal,
-    IonButton,
   ],
 })
-export class AboutPage {
-  isModalOpen = false;
-  modalTitle = '';
-  modalContent = '';
-
-  openModal(interest: string) {
-    this.isModalOpen = true;
-    switch (interest) {
-      case 'exercise':
-        this.modalTitle = 'Exercise';
-        this.modalContent = 'I enjoy strength training and cardio at the gym.';
-        break;
-      case 'tableTennis':
-        this.modalTitle = 'Table Tennis';
-        this.modalContent =
-          'I regularly play competitive table tennis with friends.';
-        break;
-      case 'gaming':
-        this.modalTitle = 'Gaming';
-        this.modalContent = 'Favorite games: Valorant, CS2, and Elden Ring.';
-        break;
-      case 'music':
-        this.modalTitle = 'Music';
-        this.modalContent =
-          'I enjoy EDM, Lo-fi beats, and instrumental soundtracks.';
-        break;
-      case 'firearms':
-        this.modalTitle = 'Firearms';
-        this.modalContent =
-          'I am interested in the mechanics and design of firearms.';
-        break;
-    }
-  }
-}
+export class AboutPage {}
