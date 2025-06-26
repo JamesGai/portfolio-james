174110c5e1edb80da08cec2fae48d2862edbe1bf
JamesGai <james1412049013@gmail.com>
2025-06-26 15:53:01 +1200

    Fixed nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { IonicModule } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
   standalone: true,
   imports: [IonicModule],
 })
